c2e347044c0bf240d0c456a40ff81b6382869c0f
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-03-28 20:24:20 -0700

    added pseudo code and plan for javascript code

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+function generatePassword() {
+    console.log("Clicked")
+
+
+// 1. Prompt the user for the password criteria.
+// (Password length between 8<128)
+// (Lowercase, uppercase, special characters)
+// 2. Validate the input
+// 3. Generate Password
+
+
+
+
+//4.Display generated password on the page.
+
+
+return "Generated Password";
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
